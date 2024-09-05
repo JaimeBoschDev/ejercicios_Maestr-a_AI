d6c447da0559284d6f4e9c0741db05221ef0746b
Jaime Bosch <jaimeboschdev@outlook.com>
2024-09-04 22:18:39 -0600

    Trabajando con Modulos y leyendo archivos

---
+++ Curso_02/02_Actividades.py
@@ -0,0 +1,9 @@
+'''
+Usando un cuaderno de Jupyter, 
+escribe las líneas de código necesarias para 
+crear un fichero llamado “managing_files.txt”
+ con “Aprendiendo a manejar ficheros con Python”. 
+'''
+
+with open("managing_files.txt", "w") as archivo:
+    archivo.write("Aprendiendo Pÿthon")
